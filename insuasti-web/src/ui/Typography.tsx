2cc465805ec6dffc89c38422599ba8a2f30c60be
Juan Insuasti <juan.insuasti@gmail.com>
2025-03-03 11:22:20 -0500

    feat: add ui components for Typography and Layout

---
+++ insuasti-web/src/ui/Typography.tsx
@@ -0,0 +1,29 @@
+import clsx from 'clsx';
+import { JSX, FC } from 'react';
+
+type TTypographyVariants = 'h1' | 'h2' | 'h3' | 'h4' | 'p' | 'small';
+
+type TTypographyProps = {
+  children: React.ReactNode;
+  className?: string;
+  as?: keyof JSX.IntrinsicElements;
+  variant: TTypographyVariants;
+};
+
+export const Typography: FC<TTypographyProps> = ({
+  children,
+  className,
+  as: Tag = 'p',
+  variant,
+}) => {
+  const baseStyles: Record<TTypographyVariants, string> = {
+    h1: 'text-left text-3xl font-bold md:text-4xl lg:text-5xl',
+    h2: 'text-left text-2xl font-bold md:text-3xl lg:text-4xl',
+    h3: 'text-left text-1xl font-bold md:text-2xl lg:text-3xl',
+    h4: 'text-left text-lg font-bold md:text-xl lg:text-2xl',
+    p: 'mt-1 text-left text-xs md:mt-2 md:text-3sm lg:mt-4 lg:text-base',
+    small: 'text-xs text-gray-500',
+  };
+
+  return <Tag className={clsx(baseStyles[variant], className)}>{children}</Tag>;
+};
