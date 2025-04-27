d7a4746e95f8b0baa624e697eb5e6a07606b8bb6
Juan Insuasti <juan.insuasti@gmail.com>
2025-04-27 11:44:24 -0500

    feat(fonts): add custom font support and improve typography

--- ui/src/ui/Typography.tsx
+++ ui/src/ui/Typography.tsx
@@ -17,12 +17,12 @@ export const Typography: FC<TTypographyProps> = ({
   variant,
 }) => {
   const baseStyles: Record<TTypographyVariants, string> = {
-    h1: 'text-left text-3xl font-bold md:text-4xl lg:text-5xl',
-    h2: 'text-left text-2xl font-bold md:text-3xl lg:text-4xl',
-    h3: 'text-left text-xl font-bold md:text-2xl lg:text-3xl',
-    h4: 'text-left text-lg font-bold md:text-xl lg:text-2xl',
-    p: 'text-left text-xs  md:text-sm lg:text-base',
-    small: 'text-xs text-gray-500',
+    h1: 'h1',
+    h2: 'h2',
+    h3: 'h3',
+    h4: 'h4',
+    p: 'p',
+    small: 'small',
   };
 
   return <Tag className={clsx(baseStyles[variant], className)}>{children}</Tag>;
