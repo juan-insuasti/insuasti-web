5a583c1f0c95a236cbc1266d42906043758f0cc9
Juan Insuasti <juan.insuasti@gmail.com>
2025-04-27 11:44:56 -0500

    refactor: improve storybook theme handling

--- ui/src/ui/Social.stories.tsx
+++ ui/src/ui/Social.stories.tsx
@@ -1,11 +1,13 @@
 import type { Meta, StoryObj } from '@storybook/react';
 import { SocialLinks } from '@/ui/SocialLinks';
 import { SOCIAL_LINKS } from '@/lib/consts';
+import { withTheme } from '@/lib/storybook-utils';
 
 export default {
   component: SocialLinks,
   title: 'Social',
   tags: ['autodocs'],
+  decorators: [withTheme('light')],
 } satisfies Meta<typeof SocialLinks>;
 
 export type Story = StoryObj<typeof SocialLinks>;
@@ -14,4 +16,11 @@ export const Default: Story = {
   args: {
     socialLinks: SOCIAL_LINKS,
   },
+};
+
+export const Dark: Story = {
+  args: {
+    socialLinks: SOCIAL_LINKS,
+  },
+  decorators: [withTheme('dark')],
 };
