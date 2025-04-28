b6c568fd135eefef0340dc936d464e2f5bdabde5
Juan Insuasti <juan.insuasti@gmail.com>
2025-04-27 20:43:57 -0500

    refactor(nav-bar): improves nav bar components and styles

--- ui/src/ui/ThemeMenu.tsx
+++ ui/src/ui/ThemeMenu.tsx
@@ -18,7 +18,7 @@ export function ThemeMenu() {
   return (
     <DropdownMenu modal={false} defaultOpen={false}>
       <DropdownMenuTrigger asChild>
-        <Button variant="outline" size="icon">
+        <Button variant="outline">
           <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
@@ -27,7 +27,7 @@ export function ThemeMenu() {
       <DropdownMenuContent align="end">
         <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
         <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>System</DropdownMenuItem>
+        <DropdownMenuItem onClick={() => setTheme('system')}>System Default</DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
--- ui/src/ui/navbar/NavBar.tsx
+++ ui/src/ui/navbar/NavBar.tsx
@@ -2,6 +2,9 @@ import React, { JSX } from 'react';
 
 import { MobileNavBar } from './MobileNavBar';
 import { DesktopNavBar } from './DesktopNavBar';
+import Link from 'next/link';
+import { Typography } from '../Typography';
+import { ThemeMenu } from '../ThemeMenu';
 
 export type NavItem = {
   title: string;
@@ -17,9 +20,19 @@ const NavItems: Array<NavItem> = [
 
 export function NavBar(): JSX.Element {
   return (
-    <header className="fixed top-0 z-50 flex h-14 w-full justify-between bg-background shadow-md dark:shadow-md dark:shadow-foreground">
-      <DesktopNavBar navItems={NavItems} />
-      <MobileNavBar navItems={NavItems} />
+    <header className="fixed inset-0 z-50 mx-auto h-14 w-full bg-background shadow-md dark:shadow-md dark:shadow-foreground">
+      <nav className="container mx-auto flex h-full items-center justify-between px-4">
+        <Link href="/" className="flex h-full items-center">
+          <Typography as="span" className="text-3xl text-primary" variant={'h2'}>
+            Insuasti
+          </Typography>
+        </Link>
+        <div className="flex items-center gap-4">
+          <ThemeMenu />
+          <DesktopNavBar navItems={NavItems} />
+          <MobileNavBar navItems={NavItems} />
+        </div>
+      </nav>
     </header>
   );
 }
