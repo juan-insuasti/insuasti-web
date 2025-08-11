c7839d5df9786ddcde022ec34c80dc5b383a03a8
Juan Insuasti <juan.insuasti@gmail.com>
2025-08-11 08:38:06 -0500

    feat(project-page): adds baseline for project pages

---
+++ ui/src/ui/icons/VercelIcon.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+
+export const VercelIcon = (props: React.SVGProps<SVGSVGElement>) => (
+  <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 24 24" width={24} height={24} {...props}>
+    <title>Vercel</title>
+    <path d="m12 1.608 12 20.784H0Z" />
+  </svg>
+);
