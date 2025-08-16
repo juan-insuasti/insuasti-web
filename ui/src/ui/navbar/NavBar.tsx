f57b3dc643e4cca5cead55c6e017d69c96382b85
Juan Insuasti <juan.insuasti@gmail.com>
2025-08-16 12:10:15 -0500

    fix(navbar): fixes logo image/text position mismatch

--- ui/src/ui/navbar/NavBar.tsx
+++ ui/src/ui/navbar/NavBar.tsx
@@ -32,11 +32,15 @@ export function NavBar(): JSX.Element {
     <header className="fixed inset-0 z-50 mx-auto h-14 w-full bg-background opacity-90 shadow-md">
       <nav className="container mx-auto flex h-full items-center justify-between px-4">
         <Link href="/" className="flex h-8 items-center">
-          <div className="flex items-end justify-end gap-4">
-            <div className="relative h-8 w-8 p-[2px]">
-              <LogoIcon className="h-8 w-8 fill-foreground" />
+          <div className="flex items-center justify-center gap-4">
+            <div className="relative flex h-10 w-10 items-center justify-center">
+              <LogoIcon className="h-9 w-9 fill-foreground" />
             </div>
-            <Typography as="span" className="text-primary sm:text-4xl/[28px]" variant={'h2'}>
+            <Typography
+              as="span"
+              className="translate-y-[2px] text-4xl text-primary"
+              variant={'h2'}
+            >
               Insuasti
             </Typography>
           </div>
