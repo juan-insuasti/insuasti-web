12aa7a6989fd11613f5ef09ba6b1bb0ea4d60643
Juan Insuasti <juan.insuasti@gmail.com>
2025-09-05 10:58:30 -0500

    feat(navbar): UI updates

--- ui/src/components/layout/navbar/NavBar.tsx
+++ ui/src/components/layout/navbar/NavBar.tsx
@@ -30,21 +30,26 @@ const NavItems: Array<NavItem> = [
 
 export function NavBar(): JSX.Element {
   return (
-    <header className="fixed inset-0 z-50 mx-auto h-14 w-full bg-background opacity-90 shadow-md">
+    <header className="fixed inset-0 mx-auto z-10 h-14 w-full bg-background/60 ">
+      <div className="absolute inset-0 h-full -z-1 w-full backdrop-blur-sm" />
       <nav className="container mx-auto flex h-full items-center justify-between px-4">
-        <Link href="/" className="flex  items-center justify-center gap-4">
-          <LogoIcon className="h-9 w-9 fill-foreground flex-auto shrink-0 grow-0" />
-          <Typography as="span" className="translate-y-[2px] text-4xl text-primary" variant={'h2'}>
-            Insuasti
-          </Typography>
-        </Link>
+        <div className="flex items-center sm:gap-12 md:gap-24">
+          <Link href="/" className="flex  items-center justify-center gap-4">
+            <LogoIcon className="h-9 w-9 fill-foreground flex-auto shrink-0 grow-0" />
+            <Typography
+              as="span"
+              className="text-3xl translate-y-[2px] sm:block: sm:text-4xl text-primary"
+              variant={'h2'}
+            >
+              Insuasti
+            </Typography>
+          </Link>
 
-        <div className="flex items-center gap-4">
           <DesktopNavBar navItems={NavItems} />
+        </div>
+        <div className="flex items-center justify-center gap-4">
+          <ThemeMenu />
           <MobileNavBar navItems={NavItems} />
-          <div className="ml-4 hidden sm:block">
-            <ThemeMenu />
-          </div>
         </div>
       </nav>
     </header>
