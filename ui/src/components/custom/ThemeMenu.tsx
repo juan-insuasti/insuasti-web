e35353f78135e15e4499571a196ea2a925803b9f
Juan Insuasti <juan.insuasti@gmail.com>
2025-09-05 10:58:30 -0500

    feat(navbar): UI updates

--- ui/src/components/custom/ThemeMenu.tsx
+++ ui/src/components/custom/ThemeMenu.tsx
@@ -5,31 +5,28 @@ import * as React from 'react';
 import { Moon, Sun } from 'lucide-react';
 import { useTheme } from 'next-themes';
 
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from '@/components/ui/dropdown-menu';
 import { Button } from '@/components/ui/shad-button';
 
 export function ThemeMenu() {
   const { setTheme } = useTheme();
 
   return (
-    <DropdownMenu modal={false} defaultOpen={false}>
-      <DropdownMenuTrigger asChild>
-        <Button variant="outline">
-          <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => setTheme('light')}>Light</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('dark')}>Dark</DropdownMenuItem>
-        <DropdownMenuItem onClick={() => setTheme('system')}>System Default</DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <div className="block">
+      <Button
+        variant="ghost"
+        size="icon"
+        aria-label="Toggle theme"
+        className="cursor-pointer"
+        onClick={() => {
+          setTheme((prevTheme) => (prevTheme === 'light' ? 'dark' : 'light'));
+        }}
+      >
+        <Sun className="rotate-0 scale-100 transition-all duration-300 dark:-rotate-90 dark:scale-0" />
+
+        <Moon className="absolute  rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100 duration-300" />
+
+        <span className="sr-only">Toggle theme</span>
+      </Button>
+    </div>
   );
 }
--- ui/src/components/layout/navbar/MobileNavBar.tsx
+++ ui/src/components/layout/navbar/MobileNavBar.tsx
@@ -11,6 +11,7 @@ import { Typography } from '@ui/Typography';
 
 import { Button } from '@/components/ui/shad-button';
 
+import { GlobalFooter } from '../GlobalFooter';
 import type { NavItem } from './NavBar';
 
 type MobileNavBarProps = {
@@ -21,26 +22,36 @@ export function MobileNavBar({ navItems }: MobileNavBarProps): JSX.Element {
   const [open, setOpen] = useState(false);
 
   return (
-    <div className="block w-full sm:hidden">
+    <div className="block sm:hidden">
       <Sheet open={open} onOpenChange={setOpen}>
         <SheetTrigger asChild>
-          <Button variant="outline" aria-label="Open menu">
-            <SquareMenu />
+          <Button variant="ghost" aria-label="Open menu" size={'icon'}>
+            <SquareMenu className="h-6 w-6" />
           </Button>
         </SheetTrigger>
-        <SheetContent title="Menu">
-          <SheetTitle className="invisible">Menu</SheetTitle>
-          <ul className="flex flex-col space-y-4 py-14 align-middle">
-            {navItems.map((item) => (
-              <li key={item.title} className="flex items-center" onClick={() => setOpen(false)}>
-                <Link href={item.href} className="hover:text-primary">
-                  <Typography variant={'h2'} as="p" className="font-semibold! text-foreground!">
-                    {item.title}
-                  </Typography>
-                </Link>
-              </li>
-            ))}
-          </ul>
+        <SheetContent title="Menu" className="p-0 ">
+          <SheetTitle className="invisible absolute">Menu</SheetTitle>
+          <div className="flex h-full flex-col justify-between">
+            <ul className="flex flex-col space-y-4 py-14 align-middle">
+              {navItems.map((item) => (
+                <li key={item.title} className="flex items-center" onClick={() => setOpen(false)}>
+                  <Link
+                    href={item.href}
+                    className="border-l-8 border-b-2 border-gray-500-muted p-4 hover:border-primary w-full"
+                  >
+                    <Typography
+                      variant={'h2'}
+                      as="p"
+                      className="font-semibold! text-foreground! hover:text-primary! transition-all"
+                    >
+                      {item.title}
+                    </Typography>
+                  </Link>
+                </li>
+              ))}
+            </ul>
+            <GlobalFooter />
+          </div>
           <ThemeMenu />
         </SheetContent>
       </Sheet>
