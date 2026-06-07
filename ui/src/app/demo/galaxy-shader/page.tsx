c388ced8e3be5be7f9ed7f09d424130c238a5153
Juan Insuasti <juan.insuasti@gmail.com>
2026-06-06 20:41:38 -0500

    feat(galaxy-shader): demo

--- ui/src/app/demo/galaxy-shader/page.tsx
+++ ui/src/app/demo/galaxy-shader/page.tsx
@@ -4,6 +4,7 @@ import React from 'react';
 
 import { Canvas } from '@react-three/fiber';
 import { Leva } from 'leva';
+import * as THREE from 'three';
 
 import GalaxyScene from './galaxy-scene';
 
@@ -11,7 +12,16 @@ export default function App() {
   return (
     <section className="h-screen w-full">
       <Leva collapsed />
-      <Canvas camera={{ position: [0, 0, 20], fov: 45, near: 0.1, far: 10000 }}>
+      <Canvas
+        camera={{
+          position: [-1.9908757513607382, -10.843449068765366, 2.8049219571559227],
+          rotation: new THREE.Euler(1.3176703947622146, -0.17591377777547543, 0.59481841932217),
+          fov: 45,
+          near: 0.1,
+          far: 10000,
+        }}
+        gl={{ antialias: true }}
+      >
         <GalaxyScene />
       </Canvas>
     </section>
