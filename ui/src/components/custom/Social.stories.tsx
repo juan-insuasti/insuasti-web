5f87365670b46ffdf45f209d5eb212393cd65a6e
Juan Insuasti <juan.insuasti@gmail.com>
2026-06-08 11:28:15 -0500

    task: update storybook

--- ui/src/components/custom/Social.stories.tsx
+++ ui/src/components/custom/Social.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 
 import { SOCIAL_LINKS } from '@lib/consts';
 import { withTheme } from '@lib/storybook-utils';
--- ui/src/components/features/blog/BlogShowcase.stories.tsx
+++ ui/src/components/features/blog/BlogShowcase.stories.tsx
@@ -1,4 +1,4 @@
-import type { Meta, StoryObj } from '@storybook/react';
+import type { Meta, StoryObj } from '@storybook/nextjs';
 
 import { BlogFilters } from '@features/blog/BlogFilters';
 import { BlogPostCard } from '@features/blog/BlogPostCard';
